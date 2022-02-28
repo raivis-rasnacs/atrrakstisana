aaed3da6f7d0c6567f61c1179fa8d6139b6f17bb
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-02-28 18:58:16 +0000

    Latviskoti mainīgo nosaukumi

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var rakstisanaSakusies = false;
-var seconds;
+var sekundes;
 var taimeris;
 
 function startTimer() {
   if (rakstisanaSakusies == false) {
-    seconds = 60;
+    sekundes = 60;
     taimeris = setInterval(tikTak, 1000);
   }
   rakstisanaSakusies = true;
 }
 
 function tikTak() {
-  seconds = seconds - 1;
-  console.log(seconds);
-  if (seconds == 0) {
+  sekundes = sekundes - 1;
+  //console.log(seconds);
+  if (sekundes == 0) {
     var text = document.getElementById("tekstaLauks").value;
     var words = text.split(" ");
     alert(words.length+" vārdi minūtē")
